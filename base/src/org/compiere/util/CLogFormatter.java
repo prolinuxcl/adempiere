fddc79dad1f2d6462f2be5b160144e14245d47b2
andcuevas <22244521+andcuevas@users.noreply.github.com>
2022-02-11 19:10:43 -0300

    New log format.

--- base/src/org/compiere/util/CLogFormatter.java
+++ base/src/org/compiere/util/CLogFormatter.java
@@ -17,305 +17,289 @@
 package org.compiere.util;
 
 import java.awt.Toolkit;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.rmi.ServerException;
 import java.sql.SQLException;
 import java.sql.Timestamp;
-import java.util.logging.Formatter;
-import java.util.logging.Handler;
-import java.util.logging.Level;
-import java.util.logging.LogRecord;
+import java.time.OffsetDateTime;
+import java.util.logging.*;
 
 
 /**
- *	Adempiere Log Formatter
- *	
- *  @author Jorg Janke
- *  @version $Id: CLogFormatter.java,v 1.2 2006/07/30 00:54:36 jjanke Exp $
+ * Adempiere Log Formatter
+ *
+ * @author Jorg Janke
+ * @version $Id: CLogFormatter.java,v 1.2 2006/07/30 00:54:36 jjanke Exp $
  */
-public class CLogFormatter extends Formatter
-{
-	/**
-	 * 	Get Formatter
-	 *	@return singleton
-	 */
-	public static synchronized CLogFormatter get()
-	{
-		if (s_formatter == null)
-			s_formatter = new CLogFormatter();
-		return s_formatter;
-	}	//	get
-	
-	/**	Singleton				*/
-	private static CLogFormatter	s_formatter = null;
-    /**	New Line				*/
-    public static String	NL = System.getProperty("line.separator");
-	
-	/**************************************************************************
-	 * 	CLogFormatter
-	 */
-	private CLogFormatter()
-	{
-		super ();
-	}	//	CLogFormatter
+public class CLogFormatter extends Formatter {
+    /**
+     * Get Formatter
+     *
+     * @return singleton
+     */
+    public static synchronized CLogFormatter get() {
+        if (s_formatter == null)
+            s_formatter = new CLogFormatter();
+        return s_formatter;
+    }    //	get
 
-	/** Short Format			*/
-	private boolean			m_shortFormat = false;
+    /**
+     * Singleton
+     */
+    private static CLogFormatter s_formatter = null;
+    /**
+     * New Line
+     */
+    public static String NL = System.getProperty("line.separator");
 
-	/**
-	 * 	Format
-	 *	@param record log record
-	 *	@return formatted string
-	 */
-	public String format (LogRecord record)
-	{
-		StringBuffer sb = new StringBuffer();
-		
-		long ms = record.getMillis();
-		Timestamp ts = null;
-		if (ms == 0)
-			ts = new Timestamp(System.currentTimeMillis());
-		else
-			ts = new Timestamp(ms);
-		String tsStr = "";
-		try
-		{
-			tsStr = ts.toString() + "00"; 
-		}
-		catch (Exception e)
-		{
-			System.err.println("CLogFormatter.format: Millis=" 
-				+ ms + " - " + e.toString() + " - " + record.getMessage());
-			//      1   5    1    5    2    5
-			tsStr = "_________________________";
-		}
-			
+    private static final SimpleFormatter simpleFormatter = new SimpleFormatter();
 
-		/**	Time/Error		*/
-		if (record.getLevel() == Level.SEVERE)
-		{	//		   12:12:12.123
-			sb.append("===========> ");
-			if (Ini.isClient())
-				Toolkit.getDefaultToolkit().beep();
-		}
-		else if (record.getLevel() == Level.WARNING)
-		{	//		   12:12:12.123
-			sb.append("-----------> ");
-		}
-		else
-		{
-			sb.append(tsStr.substring(11, 23));
-			int spaces = 11;
-			if (record.getLevel() == Level.INFO)
-				spaces = 1;
-			else if (record.getLevel() == Level.CONFIG)
-				spaces = 3;
-			else if (record.getLevel() == Level.FINE)
-				spaces = 5;
-			else if (record.getLevel() == Level.FINER)
-				spaces = 7;
-			else if (record.getLevel() == Level.FINEST)
-				spaces = 9;
-			sb.append("                          ".substring(0, spaces));
-		}
-		
-		/**	Class.method	**/
-		if (!m_shortFormat)
-			sb.append(getClassMethod(record)).append(": ");
-		
-		/**	Message			**/
-		sb.append(record.getMessage());
-		/** Parameters		**/
-		String parameters = getParameters(record);
-		if (parameters.length() > 0)
-			sb.append(" (").append(parameters).append(")");
-		
-		/**	Level			** 
-		sb.append(" ")
-			.append(record.getLevel().getLocalizedName());
-		/**	Thread			**/
-		if (record.getThreadID() != 10)
-			sb.append(" [").append(record.getThreadID()).append("]");
-		
-		//
-		sb.append(NL);
-		if (record.getThrown() != null) 
-	        sb.append(getExceptionTrace(record)).append(NL);
-		return sb.toString();
-	}	//	format
+    /**************************************************************************
+     * 	CLogFormatter
+     */
+    private CLogFormatter() {
+        super();
+    }    //	CLogFormatter
 
-	
+    /**
+     * Format
+     *
+     * @param record log record
+     * @return formatted string
+     */
+    public String format(LogRecord record) {
+
+//        11-Feb-2022 12:42:57.100 INFO [main] org.apache.catalina.startup.Catalina.load Initialization processed in 1610 ms
+        String message = OffsetDateTime.now() + " " + record.getLevel() + " [" + record.getThreadID() + "] "
+                + record.getSourceClassName() + " - " + record.getMessage() + NL;
+
+        Throwable thrown = record.getThrown();
+
+        if (thrown != null) {
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            thrown.printStackTrace(pw);
+            message = message + sw;
+        }
+
+        return message;
+
+//        StringBuilder sb = new StringBuilder();
+//
+//        long ms = record.getMillis();
+//        Timestamp ts;
+//        if (ms == 0)
+//            ts = new Timestamp(System.currentTimeMillis());
+//        else
+//            ts = new Timestamp(ms);
+//        String tsStr;
+//        try {
+//            tsStr = ts + "00";
+//        } catch (Exception e) {
+//            System.err.println("CLogFormatter.format: Millis="
+//                    + ms + " - " + e.toString() + " - " + record.getMessage());
+//            //      1   5    1    5    2    5
+//            tsStr = "_________________________";
+//        }
+//
+//
+//        /*    Time/Error		*/
+//        if (record.getLevel() == Level.SEVERE) {    //		   12:12:12.123
+//            sb.append("===========> ");
+//            if (Ini.isClient())
+//                Toolkit.getDefaultToolkit().beep();
+//        } else if (record.getLevel() == Level.WARNING) {    //		   12:12:12.123
+//            sb.append("-----------> ");
+//        } else {
+//            sb.append(tsStr.substring(11, 23));
+//            int spaces = 11;
+//            if (record.getLevel() == Level.INFO)
+//                spaces = 1;
+//            else if (record.getLevel() == Level.CONFIG)
+//                spaces = 3;
+//            else if (record.getLevel() == Level.FINE)
+//                spaces = 5;
+//            else if (record.getLevel() == Level.FINER)
+//                spaces = 7;
+//            else if (record.getLevel() == Level.FINEST)
+//                spaces = 9;
+//            sb.append("                          ".substring(0, spaces));
+//        }
+//
+//        /*    Class.method	*/
+//        /*
+//         * Short Format
+//         */
+//        sb.append(getClassMethod(record)).append(": ");
+//
+//        /*    Message			*/
+//        sb.append(record.getMessage());
+//        /* Parameters		*/
+//        String parameters = getParameters(record);
+//        if (parameters.length() > 0)
+//            sb.append(" (").append(parameters).append(")");
+//
+//        /*    Level			*
+//         sb.append(" ")
+//         .append(record.getLevel().getLocalizedName());
+//         *	Thread			*/
+//        if (record.getThreadID() != 10)
+//            sb.append(" [").append(record.getThreadID()).append("]");
+//
+//        sb.append(NL);
+//        if (record.getThrown() != null)
+//            sb.append(getExceptionTrace(record)).append(NL);
+//        return sb.toString();
+    }    //	format
+
+
     /**
      * Return the header string for a set of formatted records.
-     * @param   h  The target handler.
-     * @return  header string
+     *
+     * @param h The target handler.
+     * @return header string
      */
-    public String getHead(Handler h) 
-    {
-		String className = h.getClass().getName();
-		int index = className.lastIndexOf('.');
-		if (index != -1)
-			className = className.substring(index+1);
-    	StringBuffer sb = new StringBuffer()
-			.append("*** ")
-			.append(new Timestamp(System.currentTimeMillis()))
-			.append(" Adempiere Log (").append(className)
-			.append(") ***").append(NL);
-    	return sb.toString(); 
-    }	//	getHead
+    public String getHead(Handler h) {
+        String className = h.getClass().getName();
+        int index = className.lastIndexOf('.');
+        if (index != -1)
+            className = className.substring(index + 1);
+        return "*** " +
+                new Timestamp(System.currentTimeMillis()) +
+                " Adempiere Log (" + className +
+                ") ***" + NL;
+    }    //	getHead
 
     /**
      * Return the tail string for a set of formatted records.
-     * @param   h  The target handler.
-     * @return  tail string
+     *
+     * @param h The target handler.
+     * @return tail string
      */
-    public String getTail(Handler h) 
-    {
-		String className = h.getClass().getName();
-		int index = className.lastIndexOf('.');
-		if (index != -1)
-			className = className.substring(index+1);
-    	StringBuffer sb = new StringBuffer()
-			.append(NL)
-			.append("*** ")
-			.append(new Timestamp(System.currentTimeMillis()))
-			.append(" Adempiere Log (").append(className)
-			.append(") ***").append(NL);
-    	return sb.toString(); 
-    }	//	getTail
-	
-    /**
-     * 	Set Format
-     *	@param shortFormat format
-     */
-    public void setFormat (boolean shortFormat)
-    {
-    	m_shortFormat = shortFormat;
-    }	//	setFormat
-    
+    public String getTail(Handler h) {
+        String className = h.getClass().getName();
+        int index = className.lastIndexOf('.');
+        if (index != -1)
+            className = className.substring(index + 1);
+        return NL +
+                "*** " +
+                new Timestamp(System.currentTimeMillis()) +
+                " Adempiere Log (" + className +
+                ") ***" + NL;
+    }    //	getTail
+
     /**************************************************************************
      * 	Get Class Method from Log Record
-     *	@param record record
-     *	@return class.method
+     *    @param record record
+     *    @return class.method
      */
-    public static String getClassMethod (LogRecord record)
-    {
-    	StringBuffer sb = new StringBuffer();
-    	String className = record.getLoggerName();
-    	if (className == null 
-			|| className.indexOf("default") != -1	//	anonymous logger
-			|| className.indexOf("global") != -1)	//	global logger
-    		className = record.getSourceClassName(); 
-		if (className != null)
-		{
-			int index = className.lastIndexOf('.');
-			if (index != -1)
-				sb.append(className.substring(index+1));
-			else
-				sb.append(className);
-		}
-		else
-		    sb.append(record.getLoggerName());
-		if (record.getSourceMethodName() != null) 
-		    sb.append(".").append(record.getSourceMethodName());
-		String retValue = sb.toString();
-		if (retValue.equals("Trace.printStack"))
-			return "";
-		return retValue;
-    }	//	getClassMethod
-    
+    public static String getClassMethod(LogRecord record) {
+        StringBuilder sb = new StringBuilder();
+        String className = record.getLoggerName();
+        if (className == null
+                || className.contains("default")    //	anonymous logger
+                || className.contains("global"))    //	global logger
+            className = record.getSourceClassName();
+        if (className != null) {
+            int index = className.lastIndexOf('.');
+            if (index != -1)
+                sb.append(className.substring(index + 1));
+            else
+                sb.append(className);
+        } else
+            sb.append(record.getLoggerName());
+        if (record.getSourceMethodName() != null)
+            sb.append(".").append(record.getSourceMethodName());
+        String retValue = sb.toString();
+        if (retValue.equals("Trace.printStack"))
+            return "";
+        return retValue;
+    }    //	getClassMethod
+
     /**
-     * 	Get Log Parameters
-     *	@param record log record
-     *	@return parameters empty string or parameters
+     * Get Log Parameters
+     *
+     * @param record log record
+     * @return parameters empty string or parameters
      */
-    public static String getParameters (LogRecord record)
-    {
-    	StringBuffer sb = new StringBuffer();
-		Object[] parameters = record.getParameters();
-		if (parameters != null && parameters.length > 0)
-		{
-			for (int i = 0; i < parameters.length; i++)
-			{
-				if (i > 0)
-					sb.append(", ");
-				sb.append(parameters[i]);
-			}
-		}
-		return sb.toString();
-    }	//	getParameters
-    
+    public static String getParameters(LogRecord record) {
+        StringBuilder sb = new StringBuilder();
+        Object[] parameters = record.getParameters();
+        if (parameters != null && parameters.length > 0) {
+            for (int i = 0; i < parameters.length; i++) {
+                if (i > 0)
+                    sb.append(", ");
+                sb.append(parameters[i]);
+            }
+        }
+        return sb.toString();
+    }    //	getParameters
+
     /**
-     * 	Get Log Exception
-     *	@param record log record
-     *	@return null if exists or string
+     * Get Log Exception
+     *
+     * @param record log record
+     * @return null if exists or string
      */
-    public static String getExceptionTrace (LogRecord record)
-    {
-    	Throwable thrown = record.getThrown(); 
-		if (thrown == null)
-			return null;
-		
-    	StringBuffer sb = new StringBuffer();
-	    try 
-	    {
-	    	/** Print Stack		**
-	        StringWriter sw = new StringWriter();
-	        PrintWriter pw = new PrintWriter(sw);
-	        thrown.printStackTrace(pw);
-	        pw.close();
-	        sb.append(sw.toString());
-	        /**	Create Stack	**/
-	        fillExceptionTrace(sb, "", thrown);
-	    } 
-	    catch (Exception ex) 
-	    {
-	    }
-		return sb.toString();
-    }	//	getException
-    
+    public static String getExceptionTrace(LogRecord record) {
+        Throwable thrown = record.getThrown();
+        if (thrown == null)
+            return null;
+
+        StringBuffer sb = new StringBuffer();
+        try {
+            /* Print Stack		*
+             StringWriter sw = new StringWriter();
+             PrintWriter pw = new PrintWriter(sw);
+             thrown.printStackTrace(pw);
+             pw.close();
+             sb.append(sw.toString());
+             /*	Create Stack	*/
+            fillExceptionTrace(sb, "", thrown);
+        } catch (Exception ex) {
+        }
+        return sb.toString();
+    }    //	getException
+
     /**
-     * 	Fill Exception Trace
-     *	@param sb string buffer
-     *	@param hdr header
-     *	@param thrown thrown
+     * Fill Exception Trace
+     *
+     * @param sb     string buffer
+     * @param hdr    header
+     * @param thrown thrown
      */
-    private static void fillExceptionTrace (StringBuffer sb, String hdr, Throwable thrown)
-    {
-    	boolean firstError = hdr.length() == 0;
-		sb.append(hdr)
-			.append(thrown.toString());
-		if (thrown instanceof SQLException)
-		{
-			SQLException ex = (SQLException)thrown;
-			sb.append("; State=").append(ex.getSQLState())
-				.append("; ErrorCode=").append(ex.getErrorCode());
-		}
-		sb.append(NL);
-		//
-		StackTraceElement[] trace = thrown.getStackTrace();
-		boolean adempiereTrace = false;
-		int adempiereTraceNo = 0;
-        for (int i=0; i < trace.length; i++)
-        {
-        	adempiereTrace = trace[i].getClassName().startsWith("org.compiere.");
-        	if (thrown instanceof ServerException	//	RMI
-        		|| adempiereTrace)
-        	{
-        		if (adempiereTrace)
-                	sb.append("\tat ").append(trace[i]).append(NL);
-        	}
-        	else if (i > 20
-        		|| (i > 10 && adempiereTraceNo > 8))
-        		break;
-        	else
-        		sb.append("\tat ").append(trace[i]).append(NL);
-        	if (adempiereTrace)
-        		adempiereTraceNo++;
+    private static void fillExceptionTrace(StringBuffer sb, String hdr, Throwable thrown) {
+        sb.append(hdr)
+                .append(thrown.toString());
+        if (thrown instanceof SQLException) {
+            SQLException ex = (SQLException) thrown;
+            sb.append("; State=").append(ex.getSQLState())
+                    .append("; ErrorCode=").append(ex.getErrorCode());
+        }
+        sb.append(NL);
+        //
+        StackTraceElement[] trace = thrown.getStackTrace();
+        boolean adempiereTrace;
+        int adempiereTraceNo = 0;
+        for (int i = 0; i < trace.length; i++) {
+            adempiereTrace = trace[i].getClassName().startsWith("org.compiere.");
+            if (thrown instanceof ServerException    //	RMI
+                    || adempiereTrace) {
+                if (adempiereTrace)
+                    sb.append("\tat ").append(trace[i]).append(NL);
+            } else if (i > 20
+                    || (i > 10 && adempiereTraceNo > 8))
+                break;
+            else
+                sb.append("\tat ").append(trace[i]).append(NL);
+            if (adempiereTrace)
+                adempiereTraceNo++;
         }
         //
         Throwable cause = thrown.getCause();
         if (cause != null)
-        	fillExceptionTrace(sb, "caused by: ", cause);
-    }	//	fillExceptionTrace
-    
-}	//	CLogFormatter
+            fillExceptionTrace(sb, "caused by: ", cause);
+    }    //	fillExceptionTrace
+
+}    //	CLogFormatter
